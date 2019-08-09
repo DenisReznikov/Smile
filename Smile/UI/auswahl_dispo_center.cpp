81d39e9e76cf2ced418d6b1bb66079605fb8f567
Denis Reznikov <denis.reznikov@gmail.com>
2019-08-09 17:08:01 +0300

    make mistake to test ci

--- Smile/UI/auswahl_dispo_center.cpp
+++ Smile/UI/auswahl_dispo_center.cpp
@@ -39,7 +39,7 @@ void Auswahl_Dispo_Center::createTable()
   ui->tableView->horizontalHeader()->setSectionResizeMode(6, QHeaderView::ResizeToContents);
 }
 
-void Auswahl_Dispo_Center::on_tableView_doubleClicked(const QModelIndex &index)
+oid Auswahl_Dispo_Center::on_tableView_doubleClicked(const QModelIndex &index)
 {
   ui->labelDispoCenter->setText(ui->tableView->model()->data(ui->tableView->model()->index(index.row(),2)).toString());
   auftrags =new Auftragsauswahl(ui->Name_person->text(),ui->labelDispoCenter->text(),this);
