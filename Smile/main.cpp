d672380e3121710eb563f73d4532823c58ffee36
Denis Reznikov <denis.reznikov@gmail.com>
2019-09-10 10:06:04 +0300

    MISTAKE!!

--- Smile/main.cpp
+++ Smile/main.cpp
@@ -1,11 +1,1 @@
-#include "UI/authentication.h"
-#include <QApplication>
-#include <QMap>
-int main(int argc, char *argv[])
-{
-  QApplication a(argc, argv);
-  Authentication auth;
-  auth.show();
-
-  return a.exec();
-}
+gg
